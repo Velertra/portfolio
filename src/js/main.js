cc29a375b4d633a3f49b74b50578fa7f86e89e74
Velertra <75776124+Velertra@users.noreply.github.com>
2023-09-05 13:12:10 -0700

    updated, still need to add functions for the buttons and style more

--- src/js/main.js
+++ src/js/main.js
@@ -3,27 +3,20 @@ import '../styles/main.scss';
 function startVid(e){
     const element = e;
 }
+//switch image to gif or vice 
 
 /* document.addEventListener('DOMContentLoaded', () => {
-    //gif still to video
-    const displayProject = document.querySelectorAll('.display-project');
-    displayProject.forEach(projectImg => projectImg.addEventListener('mouseover', () => {
-        displayProject.src = '../src/img/fornitegif.gif'
-        })
-    );
-   // displayProject.addEventListener('mouseleave', () => {displayProject.src = '../src/img/fornite_ss.png'})
-}) */
-
-document.addEventListener('DOMContentLoaded', () => {
     const displayProject = document.querySelectorAll('#display-project');
     displayProject.forEach(projectImg => {
         const stillImg = '../src/img/fortnite_ss.png';
         const movingImg = '../src/img/fortnite.gif';
-        projectImg.addEventListener('mouseover', () => {
+        projectImg.addEventListener('mouseleave', () => {
             projectImg.src = movingImg;
         })
-        projectImg.addEventListener('mouseleave', () => {
+        projectImg.addEventListener('mouseover', () => {
             projectImg.src = stillImg;
         })
     })
 });
+ */
+
