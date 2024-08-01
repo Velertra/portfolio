3b9f453d3584ba64b7f49732770899729e4f3487
Velertra <75776124+Velertra@users.noreply.github.com>
2024-07-31 17:22:09 -0700

    added new project

--- README.md
+++ README.md
@@ -4,12 +4,20 @@
 
 to publish to gh-pages:
 
+
+in webpack.config change path to "docs" when publishing to gh-pages.
+
+then in the terminal run: 
+
 npm run deploy
 
-for development: 
 
-npx webpack --watch
-
-in webpack.config change "path" to "docs" when publishing to gh-pages.
+## development
+1. start the project, run in portfolio folder:
+   - npx webpack --watch
+2. open in browser:
+   - Drag index.html to browser or 
+   copy location of index file(../portfolio/dist/index.html).
+   
 
 "dist" for development
--- src/js/gifFucntion.js
+++ src/js/gifFucntion.js
@@ -4,20 +4,24 @@
         element.addEventListener('mouseenter', function() {
             
             if(element.className == 'project-preview-1'){
-                element.src = './public/cardmemoryss.png'
+                element.src = './public/watchnrategifstill.png'
             } else if(element.className == 'project-preview-2'){
-                element.src = './public/cvbss.png'
+                element.src = './public/cardmemoryss.png'
             } else if(element.className == 'project-preview-3'){
                 element.src = './public/ucss.png'
+            } else if(element.classList == 'project-preview-4'){
+                element.src = './public/cvbss.png'
             }
         })
         element.addEventListener('mouseleave', function(){
             if(element.classList == 'project-preview-1'){
-                element.src = './public/card_memory_screen.gif'
+                element.src = './public/watchnrategif.gif'
             } else if(element.classList == 'project-preview-2'){
-                element.src = './public/cvbscreen.gif'
+                element.src = './public/card_memory_screen.gif'
             } else if(element.classList == 'project-preview-3'){
                 element.src = './public/under.gif'
+            } else if(element.classList == 'project-preview-4'){
+                element.src = './public/cvbscreen.gif'
             }
         })
     });
